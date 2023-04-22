2528b772c4c806eb1f628b8498bb5d7ccdf1f069
stobertonf <stoberton.f@lyncas.net>
2023-04-22 18:19:25 -0300

    feat(SnackBar): Adding Save Last Zip Function.

--- lib/services/ceps_services.dart
+++ lib/services/ceps_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:via_cep/models/ceps.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CepService {
   static Future<Cep> fetchCep({required String cep}) async {
@@ -10,6 +11,11 @@ class CepService {
       return Cep.fromJson(decodedJson);
     } else {
       throw Exception('Requisição inválida!');
-    }
+    
+  }
+
+   static Future<void> saveLastCep(String cep) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('lastCep', cep);
   }
 }

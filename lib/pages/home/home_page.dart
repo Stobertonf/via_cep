988ca86d9cb787de372ca73c03a822943aa2f269
stobertonf <stoberton.f@lyncas.net>
2023-04-22 13:29:41 -0300

    feat(Home-Page): Creating the searchCep textField.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,16 +1,48 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:via_cep/services/ceps_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final _searchCepController = TextEditingController();
+  bool _loading = false;
+  bool _enableField = true;
+  late String _result;
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Consultar CEPS'),
+      ),
+      body: SingleChildScrollView(
+        padding:const  EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _searchCep(),
+           
+          ],
+        ),
+      ),
+    );
   }
+Widget _searchCep() {
+    return TextField(
+      autofocus: true,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.done,
+      decoration: const InputDecoration(labelText: 'CEP'),
+      controller: _searchCepController,
+      enabled: _enableField,
+    );
+  }
+ 
 }

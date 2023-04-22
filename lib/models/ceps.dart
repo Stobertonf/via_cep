cfe781e49f8c5c12c85344e3e70fed2180ecd1e8
stobertonf <stoberton.f@lyncas.net>
2023-04-22 13:17:54 -0300

    feat(APIS): Creating the ceps model

---
+++ lib/models/ceps.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+class Ceps {
+  String cep;
+  String logradouro;
+  String complemento;
+  String bairro;
+  String localidade;
+  String uf;
+  String unidade;
+  String ibge;
+  String gia;
+
+  Ceps({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.unidade,
+    required this.ibge,
+    required this.gia,
+  });
+
+  factory Ceps.fromJson(String str) => Ceps.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Ceps.fromMap(Map<String, dynamic> json) => Ceps(
+        cep: json["cep"],
+        logradouro: json["logradouro"],
+        complemento: json["complemento"],
+        bairro: json["bairro"],
+        localidade: json["localidade"],
+        uf: json["uf"],
+        unidade: json["unidade"],
+        ibge: json["ibge"],
+        gia: json["gia"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "cep": cep,
+        "logradouro": logradouro,
+        "complemento": complemento,
+        "bairro": bairro,
+        "localidade": localidade,
+        "uf": uf,
+        "unidade": unidade,
+        "ibge": ibge,
+        "gia": gia,
+      };
+}

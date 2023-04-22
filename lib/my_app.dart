cec97de74d52bf076870081a0e2103a024f4f965
stobertonf <stoberton.f@lyncas.net>
2023-04-22 16:17:22 -0300

    feat(Home-Page): Creating My App file.

---
+++ lib/my_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:via_cep/pages/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
